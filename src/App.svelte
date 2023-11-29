1bf96e491752b27d348f768732cf44c41adc4186
MadMrMeeseeks <95554942+MadMrMeeseeks@users.noreply.github.com>
2023-11-28 21:32:19 -0500

    First Update

--- src/App.svelte
+++ src/App.svelte
@@ -7,22 +7,39 @@
         [true, true, true, false]
     ];
 
+    function flipRoomIfExists(row: number, col: number) {
+        if (row < 0 || row > 3 || col < 0 || col > 3) {
+            return;
+        }
+        else {
+            mapLayout[row][col] = !mapLayout[row][col];
+        }
+    }
+
+function roomClick(row: number, col: number) {
+
+    flipRoomIfExists(row, col);
+    flipRoomIfExists(row + 1, col);
+    flipRoomIfExists(row - 1, col);
+    flipRoomIfExists(row, col + 1);
+    flipRoomIfExists(row, col - 1);
+
+}
 
 </script>
 
 <main>
     <table>
         <tbody>
-        {#each mapLayout as row}
+        {#each mapLayout as row, rowIndex}
             <tr>
-                {#each row as cell}
-                    <td class={cell ? "defaultroomstyle":""}></td>
+                {#each row as cell, cellIndex}
+                    <td class={cell ? "defaultroomstyle":""} on:click={() => roomClick(rowIndex, cellIndex)}></td>
                 {/each}
             </tr>
         {/each}
         </tbody>
     </table>
-
 </main>
 
 <style>
@@ -34,6 +51,7 @@
     td {
         width: 75px;
         height: 75px;
+        border: medium solid white;
     }
 
 
